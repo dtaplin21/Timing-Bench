45befaace1d0d2392bc93c6e64ceddcaf49af125
Dom <dtaplin21@aol.com>
2023-06-01 15:58:44 -0700

    timing3

--- phase-3.js
+++ phase-3.js
@@ -1,16 +1,31 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
+  function addNums10(increment) {
+    const start = date.now()
+    let arr = [];
+   for(let i = 1; i <= 10; i++) {
+    arr.push(addNums(i * increment))
+    const end = date.now();
+    console.log(end - start)
+   }
+   return arr
+  } // Copy your `addNums10` code here
   // Then, add timing code
-
-  // Your code here
-
 }
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
+  function addManyNums10(increment) {
+    const start = date.now()
+    let arr = [];
+    for(let i = 1; i <= 10; i++) {
+      arr.push(addManyNums(i * increment))
+    const end = date.now();
+    console.log(end - start)
+    }
+    return arr
+    }// Copy your `addManyNums10` code here
 // Then, add timing code
 
   // Your code here
