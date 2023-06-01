5eac74f429f75273b689a79b389e914c1a4548b4
Dom <dtaplin21@aol.com>
2023-06-01 11:51:19 -0700

    timing

--- phase-1.js
+++ phase-1.js
@@ -1,14 +1,22 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
-
+  let sum = 0;
+  for(let i = 1; i <= n; i++) {
+    sum += i
+  }
+  return sum;
 }
 
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
-
+  console.time(addManyNums)
+ let count = 0
+ for(let i = 1; i <= n; i++) {
+  count += addNums(i)
+ }
+ console.timeEnd(addManyNums)
+return count
 }
 
 
